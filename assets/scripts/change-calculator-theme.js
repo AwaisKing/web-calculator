6fe0663d44127a753a0c40530e9bcc09474eaee2
AWAiS <5278488+AwaisKing@users.noreply.github.com>
2023-02-27 20:31:06 +0500

    Update change-calculator-theme.js

--- assets/scripts/change-calculator-theme.js
+++ assets/scripts/change-calculator-theme.js
@@ -1,18 +1,23 @@
 /* Script para alterar o tema da Calculadora */
+(() => {
+    darkModeEnable = document.querySelector('.theme-switch input[type="checkbox"]');
 
-darkModeEnable = document.querySelector('.theme-switch input[type="checkbox"]');
-
-function changeCalculatorTheme(data) {
-    if (data.target.checked) {
-        document.documentElement.setAttribute("data-theme", "dark");
-    } else {
-        document.documentElement.setAttribute("data-theme", "light");
+    function changeCalculatorTheme(data) {
+        if (data.target.checked) {
+            document.documentElement.setAttribute("data-theme", "dark");
+        } else {
+            document.documentElement.setAttribute("data-theme", "light");
+        }
     }
-}
 
-darkModeEnable.addEventListener("change", changeCalculatorTheme, false);
+    darkModeEnable.addEventListener("change", changeCalculatorTheme);
 
-if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) darkModeEnable.checked = true;
-window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", event => {
-    darkModeEnable.checked = event.matches;
-});
+    if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        darkModeEnable.checked = true;
+        changeCalculatorTheme(darkModeEnable);
+    }
+    window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", event => {
+        darkModeEnable.checked = event.matches;
+        changeCalculatorTheme(darkModeEnable);
+    });
+})();
